a520055a341e459ce4805355267e38901ece3692
Aditya Bhardwaj <aditya.bhardwaj@datatheta.com>
2025-06-12 15:24:47 +0530

    new dev

--- app/routers/remark_router.py
+++ app/routers/remark_router.py
@@ -1,35 +1,30 @@
-from typing import List
 from fastapi import APIRouter, Form, HTTPException
-from app.services.remark_service import insert_multiple_remarks
+from app.services.remark_service import insert_remark
 
 router = APIRouter()
 
 @router.post("/remark")
 def create_remark(
     user_id: int = Form(...),
-    remark_ids: List[int] = Form(...),
-    remark_category_ids: List[int] = Form(...),
+    remark_id: int = Form(...),
+    remark_category_id: int = Form(...),
     item_id: str = Form(...),  
     loc_id: str = Form(...),  
     status: str = Form(...),
-    oee: float = Form(...)
+    oee: float = Form(...),
+    problem: str = Form(...)
 ):
     try:
-        if len(remark_ids) != len(remark_category_ids):
-            raise HTTPException(status_code=400, detail="Each remark must have a corresponding category.")
-
-        inserted_ids = insert_multiple_remarks(
+        inserted_id = insert_remark(
             user_id=user_id,
-            remark_ids=remark_ids,
-            remark_category_ids=remark_category_ids,
+            remark_id=remark_id,
+            remark_category_id=remark_category_id,
             item_id=item_id,
             loc_id=loc_id,
             status=status,
-            oee=oee
+            oee=oee,
+            problem=problem
         )
-        return {
-            "message": "Remarks inserted successfully",
-            "inserted_remark_ids": inserted_ids
-        }
+        return {"message": "Remark inserted successfully", "remark_id": inserted_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
